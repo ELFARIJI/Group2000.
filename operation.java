0a11226bd83d699bb3609460c8bfc452f5fbae00
ELFARIJI <167411975+ELFARIJI@users.noreply.github.com>
2024-04-22 13:02:56 +0300

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_statement;
+
+/**
+ *
+ * @author hp
+ */
+public class operation {
+    int a;
+    int b;
+    
+    void add(){
+      int sum;
+      sum=a+b;
+      System.out.println("sum is :" +sum);
+    }
+    void sub(){
+      int sub;
+      sub=a-b;
+      System.out.println("sub is :" +sub );  
+        
+    }
+    void mult(){
+        int mult;
+      mult=a*b;
+      System.out.println("mult is:" +mult);
+        
+    }
+    void div(){
+        float div;
+      div=a/b;
+      System.out.println("div is:" +div);
+        
+    }
+}
