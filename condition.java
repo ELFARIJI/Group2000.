bcf662c01d0184d5fa96b1ee63bccc356de9831a
Seif2024-seif <167521789+Seif2024-seif@users.noreply.github.com>
2024-04-22 14:20:49 +0300

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_statement;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class condition {
+    
+    int num = 0;
+    void check(){
+        if (num%2==0){
+            System.out.println(" number is even\n");
+        }
+        else{
+            System.out.println("the number is odd\n");
+            
+        }
+        
+    }
+    
+}
