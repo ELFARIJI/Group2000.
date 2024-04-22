ef49833c2ea5c9a1dc4da020f72bf8a3f7d42140
ELFARIJI <167411975+ELFARIJI@users.noreply.github.com>
2024-04-22 13:08:02 +0300

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditional_statement;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class condition {
+    
+    int num = 0;
+    void iseven(){
+        if (num%2==0){
+            System.out.print(" number is even");
+        }
+        else{
+            System.out.print("the number is odd");
+            
+        }
+        
+    }
+    
+}
